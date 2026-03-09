77b950512ab3d78ee91333cced17ef549e527fbb
BrayanRValenciaR <bvalenciaguzman33@gmail.com>
2026-03-09 19:00:39 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "runner-v2";
+const CACHE = "Mothcoin-v3";
 const ASSETS = ["/", "/index.html", "/game.js", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (e) => {
